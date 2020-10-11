754ee88ed7d2d867f2f6a9387227f980447beb70
Anand Simmy <anandsimmy7@gmail.com>
2020-10-11 08:24:06 +0530

    added gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
